fb6de8d38dcaa0d59602ea8c23f729bd15854b81
Joseph Emmanuel Balmes <154301904+jblms00@users.noreply.github.com>
2025-08-22 15:02:31 +0800

    Delete js directory

--- js/usr/formatDateOnly.js
+++
@@ -1,6 +0,0 @@
-function formatDateOnly(datetime) {
-	const date = new Date(datetime);
-	const dateOptions = { year: "numeric", month: "long", day: "numeric" };
-
-	return date.toLocaleDateString("en-US", dateOptions);
-}
--- js/usr/getPassingStatus.js
+++
@@ -1,6 +0,0 @@
-function getPassingStatus(score, passingPercentage, totalItems) {
-	pointsPerItem = 2;
-	maxPoints = totalItems * pointsPerItem;
-	requiredScore = (passingPercentage / 100) * maxPoints;
-	return score >= requiredScore ? "Passed" : "Failed";
-}
